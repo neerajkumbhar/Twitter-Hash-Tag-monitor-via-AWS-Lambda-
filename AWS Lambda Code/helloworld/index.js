ffd48f7b806ea2c45e664c85a18ee668ca740562
mbarve31 <mbarve@live.com>
2015-05-02 14:04:40 -0700

    Removed unwanted code from index.js

--- AWS Lambda Code/helloworld/index.js
+++ AWS Lambda Code/helloworld/index.js
@@ -18,14 +18,10 @@ var collection = db.collection('twitter_data');
    // var doc1 = {'hello':'doc1'};
     //  var twitter_doc = {'Key1':event.key1,'Key2':event.key2,'Key3':event.key3};
   var twitter_doc = {'UserID':event.UserID,'UserName':event.UserName,'User_Follower_Count':event.User_Follower_Count,'Source':event.Source,'Location':event.Location,'Tweet_Count':event.Tweet_Count,'Tweet_Text':event.Tweet_Text,'ReTweetCount':event.ReTweetCount};
-   // var doc2 = {'hello':'doc2'};
-   // var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
-
+  
     collection.insert(twitter_doc);
 
-    //collection.insert(doc2, {w:1}, function(err, result) {});
-
-//    collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+   
 
 });
 
@@ -35,5 +31,5 @@ console.log('AWS EC2 version is',AWS.EC2.apiVersions)
     console.log('my ap value2 =', event.key2);
     console.log('my app pankaj value3 =', event.key3);
     context.succeed(event.key1);  // Echo back the first key value
-    // context.fail('Something went wrong');
+   
 };
