62360355fb805be7104714dfa57de8dd9302230c
mbarve31 <mbarve@live.com>
2015-05-12 21:21:55 -0700

    Added the code to get the Payload from Java Scheduler

--- AWS Lambda Code/helloworld/index.js
+++ AWS Lambda Code/helloworld/index.js
@@ -1,40 +1,72 @@
 
 /*
-Author: Madhura Barve
-Date: 1 April 2015
+
+Name :Madhura Barve
+Date : 1 Apr 2015
 
 */
 
 console.log('Loading function');
 
+
+
 exports.handler = function(event, context) {
 
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
-MongoClient.connect("mongodb://pdighe:Dar21968@ds049171.mongolab.com:49171/poll", function(err, db) {
-    if(!err) {
-        console.log("We are connected ..Pankaj..Cheers");
-    }else{
-        console.log("We are sorry sir")
+var database;
 
-}
+//var url = 'mongodb://pdighe:Dar21968@ds049171.mongolab.com:49171/poll';
+var url = 'mongodb://NehaWani:NehaWani@ds061268.mongolab.com:61268/cmpe273';
 
-var collection = db.collection('twitter_data');
-   
-    
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+database=db;
+  //db.close();
+var collection = db.collection('event');
+
+
   var twitter_doc = {'UserID':event.UserID,'UserName':event.UserName,'User_Follower_Count':event.User_Follower_Count,'Source':event.Source,'Location':event.Location,'Tweet_Count':event.Tweet_Count,'Tweet_Text':event.Tweet_Text,'ReTweetCount':event.ReTweetCount};
-  
-    collection.insert(twitter_doc);
 
-   
+
+
+
+var str='';
+
+str=''+event.key3;
+
+console.log(event.Users.length);
+
+
+var length=event.Users.length;
+
+/*for(var i = 0; i &lt; length; i++) {
+
+console.log(event.Users[i].name+" "+event.Users[i].value);
+collection.update({"_id":event.TagID,"users.name":event.Users[i].name},{$set:{"users.$.value":event.Users[i].value}});
+
+}*/
+
+
+
+
+
+//collection.update({"_id":event.TagID,"users.name":event.TagName},{$set:{"users.$.value":event.TagValue}});
+
+context.succeed(event.TagID);
 
 });
 
- 
 
-    console.log('My app value1 =', event.key1);
-    console.log('my ap value2 =', event.key2);
-    console.log('my app pankaj value3 =', event.key3);
-    context.succeed(event.key1);  
-   
+
+
+
+    console.log('value1 =', event.TagID);
+    console.log('value2 =', event.TagName);
+    console.log('value3 =', event.TagValue);
+   // context.succeed(event.key1);  // Echo back the first key value
+   //  context.fail('Something went wrong');
 };
