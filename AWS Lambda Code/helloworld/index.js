ff2ee2d46bd7d302740505ecf5d7df749659f9f1
mbarve31 <mbarve@live.com>
2015-05-12 21:15:14 -0700

    Refined the index.js file and removed the unnecessary code

--- AWS Lambda Code/helloworld/index.js
+++ AWS Lambda Code/helloworld/index.js
@@ -1,11 +1,16 @@
+
+/*
+Author: Madhura Barve
+Date: 1 April 2015
+
+*/
+
 console.log('Loading function');
 
 exports.handler = function(event, context) {
 
 var MongoClient = require('mongodb').MongoClient;
-//var AWS = require("aws-sdk");
 
-// Connect to the db
 MongoClient.connect("mongodb://pdighe:Dar21968@ds049171.mongolab.com:49171/poll", function(err, db) {
     if(!err) {
         console.log("We are connected ..Pankaj..Cheers");
@@ -15,8 +20,8 @@ MongoClient.connect("mongodb://pdighe:Dar21968@ds049171.mongolab.com:49171/poll"
 }
 
 var collection = db.collection('twitter_data');
-   // var doc1 = {'hello':'doc1'};
-    //  var twitter_doc = {'Key1':event.key1,'Key2':event.key2,'Key3':event.key3};
+   
+    
   var twitter_doc = {'UserID':event.UserID,'UserName':event.UserName,'User_Follower_Count':event.User_Follower_Count,'Source':event.Source,'Location':event.Location,'Tweet_Count':event.Tweet_Count,'Tweet_Text':event.Tweet_Text,'ReTweetCount':event.ReTweetCount};
   
     collection.insert(twitter_doc);
@@ -26,10 +31,10 @@ var collection = db.collection('twitter_data');
 });
 
  
-console.log('AWS EC2 version is',AWS.EC2.apiVersions)
+
     console.log('My app value1 =', event.key1);
     console.log('my ap value2 =', event.key2);
     console.log('my app pankaj value3 =', event.key3);
-    context.succeed(event.key1);  // Echo back the first key value
+    context.succeed(event.key1);  
    
 };
